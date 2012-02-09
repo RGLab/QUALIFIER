c8fe0ec87328a3b262d3302067dbf0c5c1d1c5d5
mikejiang <wjiang2@fhcrc.org>
2012-02-09 13:04:43 -0800

    remove paper folder

--- inst/doc/paper/figure.R
+++
@@ -1,7 +0,0 @@
-postscript("~/rglab/workspace/flowQA/inst/doc/paper/image/gatingHierarchy.ps")
-plot(G[[1]])
-dev.off()
-epstopdf atingHierarchy.ps
-pdf("RBCLysis.pdf",width=800,height=600)
-plot(qaTask.list[["RBCLysis"]])
-dev.off()
